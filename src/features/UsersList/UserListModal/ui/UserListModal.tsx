2ab50a5d85fc597fe73f20fb8c078295e9f66c0b
KateZzh <ekaterin.pro2@gmail.com>
2025-05-14 14:40:50 +0300

    IC-103 feat: add unblock user

---
+++ src/features/UsersList/UserListModal/ui/UserListModal.tsx
@@ -0,0 +1,35 @@
+import { DialogClose, Modal } from '@/shared/ui/modal'
+import s from './UserListModal.module.scss'
+import { Typography } from '@/shared/ui/typography'
+import { Button } from '@/shared/ui/button'
+import { useTranslations } from 'next-intl'
+
+type Props = {
+  trigger: React.ReactNode
+  title: string
+  description: string
+  onClick: () => void
+}
+
+export const UserListModal = ({ trigger, title, description, onClick }: Props) => {
+  const t = useTranslations('usersList')
+
+  return (
+    <Modal title={title} className={s.modal} trigger={trigger}>
+      <div className={s.container}>
+        <Typography variant="regular_text_16">{description}</Typography>
+
+        <div className={s.buttonsContainer}>
+          <DialogClose asChild>
+            <Button variant="outline" className={s.button}>
+              {t('no')}
+            </Button>
+          </DialogClose>
+          <Button variant="primary" className={s.button} onClick={onClick}>
+            {t('yes')}
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
