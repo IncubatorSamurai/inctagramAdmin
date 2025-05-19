36bc0e3b45a21098ae85bc17ff0c9f13c3a2f346
veronikachernobai <bombeze13@gmail.com>
2025-05-19 19:19:34 +0300

    UC-fix RemoveUserModal

--- src/features/UsersList/DropdownUsers/DropdownUsers.tsx
+++ src/features/UsersList/DropdownUsers/DropdownUsers.tsx
@@ -1,17 +1,14 @@
-import { client } from '@/app/_providers/apollo-client'
-import { RemoveUserModal } from '@/features/UsersList/RemoveUserModal/RemoveUserModal'
 import { MoreHorizontalIcon } from '@/shared/assets/icons/MoreHorizontalIcon'
-import { PersonRemoveIcon } from '@/shared/assets/icons/PersonRemoveIcon'
 import { PATH } from '@/shared/config/routes'
-import { UserBan, useRemoveUserMutation } from '@/shared/graphql'
+import { UserBan } from '@/shared/graphql'
 import { Button } from '@/shared/ui/button'
 import { Dropdown } from '@/shared/ui/dropdown'
 import { Typography } from '@/shared/ui/typography'
 import { useTranslations } from 'next-intl'
-import { toast } from 'react-toastify'
 import s from './DropdownUsers.module.scss'
 import { UnBanUserModal } from '@/features/UsersList/UnBanUserModal'
 import { BanUserModal } from '@/features/UsersList/BanUserModal'
+import { RemoveUserModal } from '@/features/UsersList/RemoveUserModal'
 
 export type Props = {
   id: number
@@ -21,34 +18,12 @@ export type Props = {
 
 export const DropdownUsers = ({ id, name, userBan }: Props) => {
   const t = useTranslations('usersList')
-  const [removeUserMutation] = useRemoveUserMutation()
-
-  const handleDelete = async () => {
-    try {
-      await removeUserMutation({ variables: { id } })
-      toast.success(t('successDeleteUser'))
-      await client.refetchQueries({
-        include: ['GetUsers'],
-      })
-    } catch {
-      toast.error(t('errorDeleteUser'))
-    }
-  }
 
   return (
     <Dropdown className={s.userList_dropdown} iconTrigger={<MoreHorizontalIcon />}>
       <ul className={s.tableDropdownList}>
         <li className={s.tableDropdownItem}>
-          <RemoveUserModal
-            trigger={
-              <Button variant="icon">
-                <PersonRemoveIcon />
-                <Typography variant="regular_text_14">{t('deleteUser')}</Typography>
-              </Button>
-            }
-            deleteUser={handleDelete}
-            name={name}
-          />
+          <RemoveUserModal name={name} id={id} />
         </li>
         <li className={s.tableDropdownItem}>
           {!userBan ? <BanUserModal name={name} id={id} /> : <UnBanUserModal name={name} id={id} />}
