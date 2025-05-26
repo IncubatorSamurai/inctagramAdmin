0e7e1d3d60d54a1895d7f06e78f35714ced05843
fedjes <salvados78@mail.ru>
2025-05-26 15:17:02 +0300

    fix IC-106

--- src/features/postList/PostList.tsx
+++ src/features/postList/PostList.tsx
@@ -7,11 +7,7 @@ import { useNormalizedPosts } from '@/shared/hooks/useGetPostsSubscription'
 
 export const PostListSub = () => {
   const t = useTranslations('search')
-  //
-  //
-
-  const { posts, loading, error, setSearchTerm, searchTerm, lastPostRef } = useNormalizedPosts()
-  if (loading) return <div>Loading...</div>
+  const { posts,  error, setSearchTerm, searchTerm, lastPostRef } = useNormalizedPosts()
   if (error) return <div>Error loading posts</div>
   return (
     <div className={s.root}>
@@ -25,40 +21,3 @@ export const PostListSub = () => {
     </div>
   )
 }
-
-//   const { data, loading, error } = useGetAllPostsQuery({
-//     variables: {
-//       endCursorPostId: 0,
-//       searchTerm: '',
-//     },
-//   })
-//   const postsFromServer = data?.getPosts.items ?? [];
-//   const {data: subscriptionData} = useGetNewPostsSubscription()
-//   console.log(subscriptionData);
-
-//  const normalizedPosts: Post[] = postsFromServer.map(item => ({
-//   id: item.id,
-//   userName: item.postOwner?.userName ?? '',
-//   description: item.description,
-//   location: '',
-//  images: item.images?.map(img => ({
-//     url: img.url ?? '',
-//     width: 0,
-//     height: 0,
-//     fileSize: 0,
-//     createdAt: '',
-//     uploadId: '',
-//   })) ?? [],
-//   createdAt: item.createdAt,
-//   updatedAt: '',
-//   ownerId: item.id,
-//   avatarOwner: item.postOwner?.avatars?.[0]?.url ?? '',
-//   owner: {
-//     id: 0, // не знаю надо ли будет
-//     userName: item.postOwner?.userName ?? '',
-//     avatarUrl: item.postOwner?.avatars?.[0]?.url ?? '',
-//   },
-//   likesCount: 0,
-//   isLiked: false,
-//   avatarWhoLikes: false,
-// }));
